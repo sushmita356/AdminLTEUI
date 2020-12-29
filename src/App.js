0829da1299fe688e50c0291111afd49f9d144d31
sushmita <mmsushma9999@gmail.com>
2020-12-29 09:52:18 +0530

    react-bootstrap pkg

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import './App.css';
-import Header from './components/Header';
-// import Sidebar from './components/Sidebar';
+import "./App.css";
+import React from "react";
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
 // import Content from './components/Content';
 
 function App() {
   return (
     <div className="App">
-      {/* <Sidebar/> */}
-      <Header/>
+      <Sidebar/>
     </div>
   );
 }
