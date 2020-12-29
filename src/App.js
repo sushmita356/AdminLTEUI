88121a03a8dfefbb1d1a1a54d1eca8ec2b245bbf
sushmita <mmsushma9999@gmail.com>
2020-12-29 09:35:12 +0530

    header added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
-// import Header from './components/Header';
-import Sidebar from './components/Sidebar';
+import Header from './components/Header';
+// import Sidebar from './components/Sidebar';
 // import Content from './components/Content';
 
 function App() {
   return (
     <div className="App">
-      <Sidebar/>
+      {/* <Sidebar/> */}
+      <Header/>
     </div>
   );
 }
